1367e0370c3dad3d70d6d489f6a1fa6dfca59913
Yunseop Kim <undefined@mealant.com>
2019-01-08 22:04:17 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,83 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+// const app = require('express')();
+import * as express from 'express';
+import * as http from 'http';
+import * as socketio from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+// http server를 socket.io server로 upgrade한다
+const io = socketio(server);
+interface IUserInfo {
+  name: string;
+  userid: string;
+  msg: string;
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+interface IMySocket extends socketio.Socket {
+  name: string;
+  userid: string;
 }
 
-// Below are examples of using TSLint errors suppression
-// Here it is suppressing missing type definitions for greeter function
+// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get('/', (req: express.Request, res: express.Response) => {
+  // tslint:disable-next-line:no-console
+  console.log(req.method)
+  res.sendFile(`${__dirname}/index.html`);
+});
 
-// tslint:disable-next-line typedef
-export async function greeter(name) {
-  // tslint:disable-next-line no-unsafe-any no-return-await
-  return await delayedHello(name, Delays.Long);
-}
+// connection event handler
+// connection이 수립되면 event handler function의 인자로 socket인 들어온다
+io.on('connection', (socket: IMySocket) => {
+  // 접속한 클라이언트의 정보가 수신되면
+  socket.on('login', (data: IUserInfo) => {
+    // tslint:disable-next-line:no-console
+    console.log(`Client logged-in:
+name: ${data.name}
+userid: ${data.userid}`);
+
+    // socket에 클라이언트 정보를 저장한다
+    socket.name = data.name;
+    socket.userid = data.userid;
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit('login', data.name);
+  });
+
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on('chat', (data: IUserInfo) => {
+    // tslint:disable-next-line:no-console
+    console.log('Message from %s: %s', socket.name, data.msg);
+
+    const msg = {
+      from: { name: socket.name, userid: socket.userid },
+      msg: data.msg,
+    };
+
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit('chat', msg);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+    // socket.emit('s2c chat', msg);
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    // io.emit('s2c chat', msg);
+
+    // 특정 클라이언트에게만 메시지를 전송한다
+    // io.to(id).emit('s2c chat', data);
+  });
+
+  // force client disconnect from server
+  socket.on('forceDisconnect', () => {
+    socket.disconnect();
+  });
+
+  socket.on('disconnect', () => {
+    // tslint:disable-next-line:no-console
+    console.log(`user disconnected: ${socket.name}`);
+  });
+});
+
+server.listen(3000, () => {
+  // tslint:disable-next-line:no-console
+  console.log('Socket IO server listening on port 3000');
+});
